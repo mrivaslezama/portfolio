2d2778c11a03b6e429ba30abcd5f3d8a2934dcf4
Miguel Rivas <mrivaslezama@gmail.com>
2024-04-10 22:17:59 -0400

    review

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,12 +1,8 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
-from rxconfig import config
-
 import reflex as rx
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction/"
-filename = f"{config.app_name}/{config.app_name}.py"
-
 
 class State(rx.State):
     """The app state."""
